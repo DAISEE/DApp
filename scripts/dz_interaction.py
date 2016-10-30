6ebe668ddf6117679f3ece1ff6cd4f4dcd4a54fd
Sam <sam@sraxrt.me>
2016-10-30 18:54:11 +0100

    improvements

--- scripts/dz_interaction.py
+++ scripts/dz_interaction.py
@@ -23,16 +23,20 @@ def getDateTime(url, data, headers):
 
 def getEnergySum(url, data, headers, t0, t1):
     sumEnergy = 0
+    timestp = 0
 
-    result = requests.post(url + '/api/4/get/watts/by_time/' + str(t0) + '/' + str(t1), headers=headers, data=data)
-    parsed_json = json.loads(result.text)
-    print('=> 1 = ' + str(parsed_json['data']))
-
-    for n in range(0, len(parsed_json['data'])):
-        watt = int(parsed_json['data'][n]['value'])
-        print('=> 2 = ' + str(parsed_json['data'][n]['value']))
-        sumEnergy += watt
-
+    try:
+        result = requests.post(url + '/api/4/get/watts/by_time/' + str(t0) + '/' + str(t1), headers=headers, data=data)
+    except json.JSONDecodeError as e:
+        print(e)
+    else :
+        parsed_json = json.loads(result.text)
+        for n in range(0, len(parsed_json['data'])):
+            # TODO : check data from citizenwatt
+            if timestp < parsed_json['data'][n]['timestamp']:
+                watt = int(parsed_json['data'][n]['value'])
+                sumEnergy += watt
+                timestp = parsed_json['data'][n]['timestamp']
     return sumEnergy
 
 
@@ -42,36 +46,38 @@ with open("parameters.yml", 'r') as stream:
     except yaml.YAMLError as e:
         print(e)
 
-# Defintion of pine used
-pine = param["usedPine"]["id"]
-pineURL = param[pine]["url"]
-pineLogin = param[pine]["login"]
-pinePswd = param[pine]["password"]
+
+# Defintion of pine used to update data
+pine = param['usedPine']['id']
+pineURL = param[pine]['url']
+pineLogin = param[pine]['login']
+pinePswd = param[pine]['password']
 headers = {'Content-Type': 'application/json', }
 data = 'login=' + pineLogin + '&password=' + pinePswd
 
 
 time0 = getDateTime(pineURL, data, headers)
-print(time0)
 
 while 1:
     # delay to define
-    time.sleep(20)
+    time.sleep(10)
     time1 = getDateTime(pineURL, data, headers)
     sumWatt = getEnergySum(pineURL, data, headers, time0, time1)
 
+    print('timestamp : ' + time.ctime(int(time1)) + ', sumWatt = ' + str(sumWatt))
+
     time0 = time1
 
-    # Consumer
-    if param[pine]['typ'] == 'C':
-        # to update Energy balance (consumer)
-        hashData = param['contract']['fctConsumeEnergy'] + padhexa(hex(sumWatt))
-        # pine2 for debug (laptop)
-        response = client.send_transaction(_from=param['pine2']['address'], to=param['contract']['address'], data=hashData)
+    if sumWatt !=0 :
 
-    # Producer
-    else:
-        # to update Energy balance (producer)
-        # pine2 for debug (laptop)
-        hashData = param['contract']['fctSetProduction'] + padhexa(hex(sumWatt))
-        response = client.send_transaction(_from=param['pine2']['address'], to=param['contract']['address'], data=hashData)
+        # Consumer
+        if param[pine]['typ'] == 'C':
+            # to update Energy balance (consumer)
+            hashData = param['contract']['fctConsumeEnergy'] + padhexa(hex(sumWatt))
+            response = client.send_transaction(_from=param['pine2']['address'], to=param['contract']['address'], data=hashData)
+
+        # Producer
+        else:
+            # to update Energy balance (producer)
+            hashData = param['contract']['fctSetProduction'] + padhexa(hex(sumWatt))
+            response = client.send_transaction(_from=param['pine2']['address'], to=param['contract']['address'], data=hashData)
